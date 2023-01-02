3131abdd5f694f7410d056cbbab21adaec835e1d
Ricardo Zamboni <ricardo-testando@hotmail.com>
2023-01-02 10:26:22 -0300

    chore: add next head to layout

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from 'react';
-
+import Head from 'next/head';
 import { Container } from './styles';
 
 type LayoutProps = {
+  title?: string;
   children: ReactNode;
 };
 
-export const DefaultLayout = ({ children }: LayoutProps) => {
+export const DefaultLayout = ({ title, children }: LayoutProps) => {
   return (
     <Container>
+      <Head>
+        <title>{title} | BU</title>
+      </Head>
+
       <main>{children}</main>
     </Container>
   );
--- src/layouts/Default/styles.ts
+++ src/layouts/Default/styles.ts
@@ -1,3 +1,10 @@
 import { styled } from 'styles';
 
-export const Container = styled('div', {});
+export const Container = styled('main', {
+  width: '100%',
+  padding: '$20 0',
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
